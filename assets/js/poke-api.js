63f043b811399e0fb08a673239de24662c4389dc
Claudinei José Santos <claudineibitta@gmail.com>
2024-02-13 23:09:00 -0300

    refactor: alteracoes nos arquivos de javascript

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,30 +1,25 @@
+const pokeApi = {};
 
-const pokeApi = {}
+function convertPokeApiDetailToPokemon(pokeApiDetail) {
+    const types = pokeApiDetail.types.map((typeSlot) => typeSlot.type.name);
+    const [type] = types;
 
-function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
-
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
-
-    pokemon.types = types
-    pokemon.type = type
-
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
-
-    return pokemon
+    return new Pokemon(
+        pokeApiDetail.id,
+        pokeApiDetail.name,
+        type,
+        types,
+        pokeApiDetail.sprites.other.dream_world.front_default
+    );
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
+    return fetch(pokemon.url).then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
     return fetch(url)
         .then((response) => response.json())
